15733afd816f17be6ecdabae0eea6b506a1545f9
Nicholas D. Socci <soccin@mskcc.org>
2024-01-22 10:48:01 -0500

    Added Sscrofa Bundle

---
+++ resources/bundle.Sscrofa11.1.sh
@@ -0,0 +1,13 @@
+export GENOME=/juno/depot/assemblies/S.scrofa/Sscrofa11.1/Sscrofa11.1.fasta
+export GENOME_INTERVALS=/home/socci/Work/Users/KimK/AulitzA/Proj_14579_B/resources/Sscrofa11.1_excludeN_100.interval_list
+export GENOME_SCATTER=/home/socci/Work/Users/KimK/AulitzA/Proj_14579_B/resources/scatter/excludeN_100
+
+export DBSNP=/juno/depot/assemblies/S.scrofa/Sscrofa11.1/sus_scrofa.vcf.gz
+export PILEUP_SNPS=/juno/work/bic/socci/Work/Users/KimK/AulitzA/Proj_14579_B/resources/Sscrofa11.1__dbSNP__30k.vcf
+export PON=/juno/res/mix/Cache/2023-09-19/14579_B/pon.vcf.gz
+
+VEPROOT=/juno/work/bic/socci/Work/Users/KimK/AulitzA/Proj_14579_B/resources/vep
+export VEPPATH=$VEPROOT/ensembl-vep-release-110
+export VEPDATA=$VEPROOT/vep_data_110
+export VEPSPECIES=sus_scrofa
+export VEPBUILD=Sscrofa11.1
